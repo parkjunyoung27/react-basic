3850e8f86999a15a0241369899a477d81aa7ea44
parkjunyoung <hpaaycim1@naver.com>
2023-04-06 20:40:38 +0900

    commit

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>Hello World</div>
   );
 }
 
-export default App;
+export default App; // 내보내는 문법 -> 사용할 땐 import 사용
+
--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// js 는 생략 가능 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactDOM.render( // React와 App을 연결해주는 기능
+  // <React.StrictMode> // 개발중에만 사용하는 모드 
+    <App />, // 원하는 최상위 컴퍼넌트 
+  // </React.StrictMode>,
+  document.getElementById('root') //위치 선택
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(); // 퍼포먼스 측정할 때 사용하는 함수 필요없음
