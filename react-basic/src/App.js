b9b201c52c392cfb88e4858f54766b76b3cf5b86
parkjunyoung <hpaaycim1@naver.com>
2023-04-08 23:40:40 +0900

    input

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,53 +1,27 @@
 import { useState } from 'react';
 
 function App(){
-  // let number = 1;
 
-  //랜더링 그때 그때 할 수 있게 
-  console.log('render');
-  const [number, setNumber] = useState(1);
-  // const [number1, setNumber1] = useState(1);
-  
-  // 1.함수
-  // const double = (number) => {
-  //   return number * 3;
-  // };
+  const [title, setTitle] = useState('');
 
-  // 2.버튼 이벤트
-  // const printHello = () => {
-  //   console.log('hello')
-  // };
-
-  // 4. useState 사용
-  const double = () => {
-    // const doubledNumber = number * 2;
-
-    // 처음 랜더링 // 간단한 문법
-    setNumber((prevState) => prevState * 2); // 중요
-
-    setNumber((prevState) => { // 업데이트 된 값이 들어옴
-      return prevState * 2
-    }); 
-
-    // setNumber1(number1 * 2); // 중요 
-    //랜더링이 맨 마지막 함수 실행까지 기다렸다 됨
-    console.log(number);
-    // console.log(number1);
-    // console.log(doubledNumber); // 업데이트 된 값을 변수에 저장해서 로그출력
-
-  };
-
-  //3. <>대신 react.fragment 사용할 수 있음 
   return (
-    <> 
-      <div>{number}</div>
-      <button className="btn btn-primary" onClick={double}>
-        Submit
-      </button>
-    </>
+    <div className="container">
+      <div className='mb-3'>
+        <label className='form-label'>Title</label>  
+        <input 
+          className='form-control'
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+      </div>
+      <button className='btn btn-primary'>
+        Post
+      </button>  
+    </div>
   );
 }
-
 
 export default App; // 내보내는 문법 -> 사용할 땐 import 사용
 
