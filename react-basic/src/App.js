eea3ed20f3b086cc6a3a209a85109524c3be3286
parkjunyoung <hpaaycim1@naver.com>
2023-04-12 23:55:41 +0900

    route 파일 만들기

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -4,30 +4,17 @@ import {
   Route
 } from 'react-router-dom'
 import NavBar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import CreatePage from './pages/CreatePage';
-import EditPage from './pages/EditPage';
-import ListPage from './pages/ListPage';
+import routes from './routes';
 
 function App(){
-
   return (
     <Router>
       <NavBar/>
       <div className="container">
         <Switch>
-          <Route path="/" exact>
-            <HomePage/>
-          </Route>
-          <Route path="/blogs" exact>
-            <ListPage/>
-          </Route>
-          <Route path="/blogs/create" exact>
-            <CreatePage/>
-          </Route>
-          <Route path="/blogs/edit" exact>
-            <EditPage/>
-          </Route>
+          {routes.map((route) => {
+            return <Route key={route.path} exact path={route.path} component={route.component} />
+          })}
         </Switch>
       </div>
     </Router>
@@ -35,3 +22,16 @@ function App(){
 }
 
 export default App; // 내보내는 문법 -> 사용할 땐 import 사용
+
+{/* <Route path="/" exact>
+  <HomePage/>
+</Route>,
+<Route path="/blogs" exact>
+  <ListPage/>
+</Route>,
+<Route path="/blogs/create" exact>
+  <CreatePage/>
+</Route>,
+<Route path="/blogs/edit" exact>
+  <EditPage/>
+</Route> */}
