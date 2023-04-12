58ae99c067cf7ad06f065fc8656837fcc463d0de
parkjunyoung <hpaaycim1@naver.com>
2023-04-13 00:23:45 +0900

    .

--- react-basic/src/components/Navbar.js
+++ react-basic/src/components/Navbar.js
@@ -1,19 +1,21 @@
 import {
-    Link
+    Link, NavLink
   } from 'react-router-dom'
 
 const NavBar = () => {
     return(
         <nav className="navbar navbar-dark bg-dark">
         <div className="container">
-         <Link className="navbar-brand" to="/">
-           Home
-         </Link>
+         <Link className="navbar-brand" to="/"> Home </Link>
            <ul className="navbar-nav">
              <li className="nav-item">
-               <Link className="nav-link active" aria-current="page" to="/blogs">
+               <NavLink
+                activeClassName="active"
+                className="nav-link" 
+                aria-current="page" 
+                to="/blogs">
                  Blogs
-               </Link>
+               </NavLink>
              </li>
            </ul>
        </div>
